02b2375c27a7594fe922f3d610bf28aa00541245
serip39 <seri@fusic.co.jp>
2022-10-24 13:18:43 +0900

    fix:setting

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,8 @@
 root = "#{Dir.getwd}"
 
 bind "tcp://0.0.0.0:7890"
-pidfile "#{root}/tmp/puma/pid"
-state_path "#{root}/tmp/puma/state"
+pidfile "#{root}/tmp/puma/server.pid"
+state_path "#{root}/tmp/puma/puma.state"
 rackup "#{root}/config.ru"
 threads 4, 8
 activate_control_app
